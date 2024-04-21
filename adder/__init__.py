91b6d77827502a238adc5bb1e92478d875f038cd
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2024-04-21 12:56:48 +0530

    Update __init__.py

--- adder/__init__.py
+++ adder/__init__.py
@@ -3,11 +3,13 @@
 
 from pyrogram import Client as adder
 from config.config import Config
+from pyromod import listen #type : ignore
 
 addboy = adder(
        "adder_boy",
        api_id=Config.API_ID,
        api_hash=Config.API_HASH,
        bot_token=Config.BOT_TOKEN,
-       plugins=dict(root="adder.modules")
+       in_memory=True,
+       plugins={'root':'adder'},
 )
